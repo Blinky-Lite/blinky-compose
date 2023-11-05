96579d82e1d1a7b7068af5068f163ab9f95bc149
David McGinnis <david.mcginnis@bl-mc.se>
2023-11-06 00:06:00 +0100

    added installDocker script

--- scripts/installDocker.sh
+++ scripts/installDocker.sh
@@ -6,19 +6,17 @@ sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
 # Add the repository to Apt sources:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 sudo usermod -aG docker $USER
 source ~/.bashrc 
 sudo docker network create tunnel 
+sudo apt install apache2-utils 
 wget https://github.com/Blinky-Lite/blinky-compose/raw/main/scripts/startPortainer.sh
 chmod +x startPortainer.sh 
 sudo echo '....Finished installing docker...exit terminal and login again' 
