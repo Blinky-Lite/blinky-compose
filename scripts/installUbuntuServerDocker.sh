c429ce2c5a1a30eaab2d3a636b03cce6f9f17c48
David McGinnis <david.mcginnis@bl-mc.se>
2023-11-06 11:42:49 +0100

    test

--- scripts/installUbuntuServerDocker.sh
+++ scripts/installUbuntuServerDocker.sh
@@ -6,10 +6,13 @@ then
     echo "exiting script"
     exit 1
 fi
+# resize file system for ubuntu
+sudo lvextend -l +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
+sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
 # Add Docker's official GPG key:
 sudo echo 'Installing Docker....'
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -18,11 +21,11 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #sudo usermod -aG docker $USER
 sudo docker network create tunnel 
-sudo apt install apache2-utils 
+sudo apt -y install apache2-utils 
 wget https://github.com/Blinky-Lite/blinky-compose/raw/main/scripts/startPortainer.sh
 chmod +x startPortainer.sh 
 ./startPortainer.sh $1
