9692834633f3934fa3956284a062d6578ea1876c
David McGinnis <david.mcginnis@bl-mc.se>
2024-10-04 05:53:51 +0200

    updated save restore databases

--- scripts/restorebackup.sh
+++ scripts/restorebackup.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
 if [ "$#"  -ne 2 ]
 then
-    echo "Usage: ./restorebackup.sh <backupName> <databasePassword>"
+    echo -e "${RED}Usage: ./restorebackup.sh <backupName> <databasePassword>${NC}"
     exit 1
 fi
-echo "Restore backup " $1
-echo "Recalling " $1 "on Google Drive"
+echo -e "${GREEN}Restore backup " $1 ${NC}
+echo -e "${GREEN}Recalling " $1 "on Google Drive${NC}"
 rclone copy  google-drive:$1 $1
 cd $1
-echo "Restoring blinky-lite_extra-img database..."
-docker run --rm --volumes-from extra-img -v $(pwd):/backup busybox sh -c "cd /data && tar xvfz /backup/blinky-lite_extra-img.tar --strip 1"
-echo "...Finished restoring blinky-lite_extra-img database" 
-echo "Restoring blinky-mongo database..."
+echo -e "${GREEN}Restoring blinky-box volumes...${NC}"
+docker run --rm --volumes-from blinky-box -v $(pwd):/backup busybox sh -c "cd /data && tar xvfz /backup/blinky-lite_extra-img.tar --strip 1"
+echo -e "${GREEN}...Finished restoring blinky-box volumes${NC}" 
+echo -e "${GREEN}Restoring blinky-mongo database...${NC}"
 tar xvfz blinky-lite_mongo-data.tar
 docker exec -i blinky-mongo /usr/bin/mongorestore --username admin --password $2 --authenticationDatabase admin --nsInclude="blinky-lite.*" --archive < mongodb
 rm mongodb
-echo "...Finished restoring blinky-lite_mongo-data database" 
+echo -e "${GREEN}...Finished restoring blinky-lite_mongo-data database${NC}" 
--- scripts/runbackup.sh
+++ scripts/runbackup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+NC='\033[0m' # No Color
 if [ "$#"  -ne 1 ]
 then
-    echo "Usage: ./runbackup.sh <databasePassword>"
+    echo -e "${RED}Usage: ./runbackup.sh <databasePassword>${NC}"
     exit 1
 fi
 remoteDirName=$(hostname)-$(date +"%y-%m-%d")
-echo "Creating backup " $remoteDirName
+echo -e "${GREEN}Creating backup " $remoteDirName ${NC}
 mkdir  $remoteDirName
 cd $remoteDirName
-echo "Creating " $remoteDirName "on Google Drive"
+echo -e "${GREEN}Creating " $remoteDirName "on Google Drive${NC}"
 rclone mkdir google-drive:$remoteDirName
-echo "Copying blinky-lite_extra-img database..."
-docker run --rm --volumes-from extra-img -v $(pwd):/backup busybox tar cvfz /backup/backup.tar /data/html-static
-echo "...Finished copying blinky-lite_extra-img database" 
+echo -e "${GREEN}Copying blinky-box volumes...${NC}"
+docker run --rm --volumes-from blinky-box -v $(pwd):/backup busybox tar cvfz /backup/backup.tar /data/html-static
+echo -e "${GREEN}...Finished copying blinky-box volumes${NC}" 
 mv backup.tar  blinky-lite_extra-img.tar
-echo "Copying blinky-lite_mongo-data database..."
+echo -e "${GREEN}Copying blinky-lite_mongo-data database...${NC}"
 docker exec -i blinky-mongo /usr/bin/mongodump --username admin --password $1 --authenticationDatabase admin --db blinky-lite --archive > mongodb
 tar cvfz blinky-lite_mongo-data.tar mongodb
 rm mongodb
-echo "...Finished copying blinky-lite_mongo-data database" 
-echo "Exporting to Google Drive..."
+echo -e "${GREEN}...Finished copying blinky-lite_mongo-data database${NC}" 
+echo -e "${GREEN}Exporting to Google Drive...${NC}"
 rclone copy . google-drive:$remoteDirName
-echo "...Finished export to Google Drive"
-echo "Finished backup " $remoteDirName
+echo -e "${GREEN}...Finished export to Google Drive${NC}"
+echo -e "${GREEN}Finished backup " $remoteDirName ${NC}
